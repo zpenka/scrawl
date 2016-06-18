d248d351410f975d71fba4b087b076eb4809c561
Zack Penka <zpenka@shutterstock.com>
2016-06-18 09:23:42 -0400

    Boilerplate tests

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,7 @@
 module.exports = {
   default: {
     client: 'mysql',
+    pool: { min: 1, max: 7 },
     connection: {
       host: '127.0.0.1',
       user: 'root',
@@ -16,6 +17,7 @@ module.exports = {
   },
   test: {
     client: 'mysql',
+    pool: { min: 1, max: 7 },
     connection: {
       host: '127.0.0.1',
       user: 'root',
@@ -31,6 +33,7 @@ module.exports = {
   },
   production: {
     client: 'mysql',
+    pool: { min: 1, max: 7 },
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/db/migrations'
--- test/helper.js
+++ test/helper.js
@@ -1,9 +1,33 @@
 const db = require('../db/knex');
 const chai = require('chai');
 const sinon = require('sinon');
+const Promise = require('bluebird');
+const _ = require('lodash');
 
 chai.config.includeStack = true;
 chai.use(require('sinon-chai'));
+
+var tables_to_clear;
+function clearTables(done) {
+  Promise.map(tables_to_clear, function(table_name) {
+    if (!table_name) {
+      console.log(`Empty table name ${table_name} in ${tables_to_clear}`);
+      throw new Error('Invalid table name: ' + table_name);
+    }
+
+    return db(table_name).del();
+  })
+  .then(() => done())
+	.catch((err) => done(err));
+}
+
+before(function(done) {
+	db.migrate.latest()
+	.then(() => done())
+	.catch((err) => done(err));
+});
+
+
 
 beforeEach(function(done) {
   process.env.NODE_ENV = 'test';
@@ -14,7 +38,21 @@ beforeEach(function(done) {
     this.sinon = sinon.sandbox.create();
   }
 
-  return db.migrate.rollback()
-  .then(() => done());
+  if (tables_to_clear) {
+    return clearTables(done);
+  }
+
+  db.raw('SHOW TABLES')
+		.then((result) => {
+
+      const all_tables  = result[0].map(function(table_data) {
+				const key = Object.keys(table_data)[0];
+				return table_data[key];
+			});
+
+      tables_to_clear = _.without(all_tables, 'knex_migrations');
+      return clearTables(done);
+    })
+		.catch((err) => done(err));
 });
 
